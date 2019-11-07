82bf3e6813ea899ce69130b049863e40120e323b
Junki Tomatsu <2018e25@edu.gifu-nct.ac.jp>
2019-11-07 22:06:08 +0900

    各画面ファイル/ログイン機能の実装

---
+++ Web/mysql.php
@@ -0,0 +1,45 @@
+<?php
+class SQL_Proc {
+    function host() {
+        return "localhost";
+    }
+
+    function port() {
+        return "3306";
+    }
+
+    function user() {
+        return "root";
+    }
+
+    function pass() {
+        return "TEST_PASSWD";
+    }
+
+    function db() {
+        return "guest_door";
+    }
+
+    function isLogin() {
+        $link = mysqli_connect($this->host() . ":" . $this->port(), $this->user(), $this->pass(), $this->db());
+        if(mysqli_connect_errno()) {
+            die("データベースに接続できません。" . mysqli_connect_error() . PHP_EOL);
+            exit;
+        }
+
+        $id = (empty($_SESSION['user_id'])) ? "" : $_SESSION['user_id'];
+        $pass = (empty($_SESSION['user_pass'])) ? "" : $_SESSION['user_pass'];;
+        $id = mysqli_real_escape_string($link, $id);
+        $pass = mysqli_real_escape_string($link, $pass);
+
+        $result = mysqli_query($link, "SELECT user, pass FROM Users WHERE user='" . $id . "' and pass=SHA1('" . $pass . "')");
+        if(!$result) {
+            die("クエリーに失敗しました。" . mysqli_error($link));
+            exit;
+        }
+
+        mysqli_close($link);
+        return (mysqli_num_rows($result) == 1);
+    }
+}
+?>
---
+++ Web/user.php
@@ -0,0 +1,96 @@
+<?php
+    include_once __DIR__ . '/mysql.php';
+    session_start();
+    if(empty($_SESSION['user_id']) || empty($_SESSION['user_pass'])) {
+        header("Location: ./login.php");
+        exit;
+    }
+
+    $sql = new SQL_Proc();
+
+    if(!$sql->isLogin()){
+        unset($_SESSION['user_id']);
+        unset($_SESSION['user_pass']);
+        header("Location: ./login.php");
+        exit;
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Guest Door - User Management</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css?a">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="./">Guest Door</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
+            aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item btn-nav">
+                    <a class="nav-link" href="./">ダッシュボード</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">ログ</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown01">
+                        <a class="dropdown-item" href="./log.php?filter=0">全てのログ</a>
+                        <a class="dropdown-item" href="./log.php?filter=1">入退室ログ</a>
+                        <a class="dropdown-item" href="./log.php?filter=2">キー管理ログ</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown active">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown02" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">管理</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown02">
+                        <a class="dropdown-item" href="./user.php">ユーザー管理 <span class="sr-only">(current)</span></a>
+                        <a class="dropdown-item" href="./key.php">キー管理</a>
+                    </div>
+                </li>
+                <li class="nav-item btn-nav">
+                    <a class="nav-link" href="./setting.php">設定</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav navbar-right">
+                <li class="nav-item">
+                    <a class="nav-link" href="./logout.php">ログアウト</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <main role="main" class="container">
+
+        <div class="starter-template">
+            <h1>Bootstrap starter template - User</h1>
+            <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text
+                and a mostly barebones HTML document.</p>
+        </div>
+
+    </main>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
